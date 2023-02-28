ec06d85623017687cfa0d90ef26707dca076de4d
AarushAarush <106853995+AarushAarush@users.noreply.github.com>
2023-02-28 18:44:51 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+objects = [];
+statuss  = "";
+video = "";
+
+function buttons(aarush) {
+    if (aarush == 1) {
+        document.getElementById("number_of_objects").style.display = 'block';
+        document.getElementById("status").style.display = 'block';
+    } else {
+        document.getElementById("number_of_objects").style.display = 'none';
+        document.getElementById("status").style.display = 'none';
+    }
+}
+
+function preload() {
+    video = createVideo('video.mp4');
+    video.hide();
+}
+
+function setup() {
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+
+function draw() {
+    image(video, 0, 0, 480, 380);
+    if(statuss != ""){
+        objectDetector.detect(video, gotResult);
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are: " + objects.length;
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function  gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects...";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
+    statuss = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
